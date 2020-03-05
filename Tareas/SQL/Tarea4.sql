8f8fc8fc0e6609b9e1c47b3d89ff457c0226ca9a
Juan Rene Soler Siri <juanr.soler_93@hotmail.es>
2020-03-04 22:30:47 -0400

    creando tabla de la pockedes

--- Tareas/SQL/Tarea4.sql
+++ Tareas/SQL/Tarea4.sql
@@ -5,10 +5,18 @@
 create database Tarea4
 use Tarea4
 
+create table Region
+(
+    Id int not null primary key identity(1,1),
+    
+)
+
 create table Pokemon
 (
     Id int not null primary key identity(1,1),
-    --
+    Nombre varchar(50) not null,
+    TipoId int not null FOREIGN KEY REFERENCES Tipo(Id),
+    RegionId int not null FOREIGN key REFERENCES Region(Id)
 )
 
 
